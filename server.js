580df34233b1a4ba789862f30a61d85a624f52d2
vinnya3 <vincent.aceto@gmail.com>
2017-06-02 22:10:18 -0400

    feat: remove console.log / change api routes name

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ import morgan from 'morgan'
 // api routes import
 import ApiRoutes from './routes/ApiRoutes'
 // define port and app
-const PORT = 8080
+const port = 8080
 const app = express()
 // setup database connection
-const db = 'mongodb://127.0.0.1/url_data'
+const db = 'mongodb://127.0.0.1/url-shortener-service'
 mongoose.connect(db)
 // set up morgan logger
 app.use(morgan('dev'))
@@ -15,13 +15,13 @@ app.use(morgan('dev'))
 app.set('views', './views')
 app.set('view engine', 'pug')
 // include api routes
-app.use('/new', ApiRoutes(app, express))
+app.use('/api', ApiRoutes(app, express))
 // main route
 app.get('*', (req, res) => {
   res.render('index', { title: 'Welcome', message: 'Hello there' })
 })
 // start server
-app.listen(PORT, (err) => {
-  if (err) console.log(err)
-  else console.log(`Listening on port: ${PORT}`)
+app.listen(port, (err) => {
+  if (err) throw new Error(err)
+  else console.log(`Listening on port: ${port}`)
 })
