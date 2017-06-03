54fd080850454a58d04fe5e39b017dbf2a0d6828
vinnya3 <vincent.aceto@gmail.com>
2017-06-02 21:57:53 -0400

    feat: remove match field off of model
    The reason being: moved validation logic into a seperate, pure
    function.  Easier to test / reuse / reason about.

--- models/UrlData.js
+++ models/UrlData.js
@@ -1,18 +1,9 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-/* eslint-disable */
-const match = [
-  /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/,
-  'Please use a valid URL! Ex: http://www.foobar.com'
-]
-/* eslint-enable */
 const urlDataSchema = new Schema({
-  url: {
-    type: String,
-    match: match
-  },
+  url: String,
   shortenedUrl: String
 })
 
-module.exports = mongoose.model('UrlData', urlDataSchema)
+export default mongoose.model('UrlData', urlDataSchema)
