d6f02bd784f82aed9365e236651e49c20918865c
vinnya3 <vincent.aceto@gmail.com>
2017-06-04 23:31:04 -0400

    feat: add new validate fn, add Result monad
    Created a validateUrl function that uses curried general validate fn.
    validateUrl uses new Result monad from folktale js. In addition,
    renamed isUrlValid fn to shortenedUrl.  shortendUrl returns Result obj.

--- controllers/ApiController.js
+++ controllers/ApiController.js
@@ -2,12 +2,16 @@
 // [] implement boolean logic for if we have/don't have
 //    the url in the database. ex: if we have, return shortUrl
 //    else, create new shortUrl, save it, then return it
-
 import _ from 'ramda'
+import Result from 'folktale/data/result'
 
 /* eslint-disable */
 const match = /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/
 /* eslint-enable */
+
+/* chain :: (ObjectA -> ObjectB), M -> ObjectB
+const chain = _.curry((fn, container) =>
+  container.chain(fn)) */
 
 // getRequestParams :: Object -> String
 const getRequestParams = req => _.prop('params', req)
@@ -15,14 +19,19 @@ const getRequestParams = req => _.prop('params', req)
 // validate :: RegEx -> String -> Boolean
 const validate = _.curry((pattern, str) => pattern.test(str))
 
+const validateUrl = _.curry(url => {
+  return validate(match, url)
+  ? Result.Ok(url) : Result.Error('Invalid Url')
+})
+
 // isUrlValid :: Object -> Boolean
-const isUrlValid = _.compose(
-  validate(match),
+const shortenedUrl = _.compose(
+  validateUrl,
   _.head,
   getRequestParams
 )
 
 export default (req, res) => {
-  const response = isUrlValid(req) ? 'Valid Url' : 'Invalid Url'
+  const response = shortenedUrl(req).merge()
   return res.render('response', { title: 'Response Page', response })
 }
