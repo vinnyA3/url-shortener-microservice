2c92a1ddea450cc4a531cc1539ffb15099b99cd2
vinnya3 <vincent.aceto@gmail.com>
2017-07-21 23:59:05 -0400

    feat: refactor to es6 export members, ramda fns

--- controllers/ApiController.js
+++ controllers/ApiController.js
@@ -1,32 +1,18 @@
 import UrlData from '../models/UrlData'
-import _ from 'ramda'
+import { compose, curry, prop, chain } from 'ramda'
 import Either from 'data.either'
 import Task from 'data.task'
 const { Left, Right } = Either
-
-
-// alt :: (Function, Function, a) -> Function(a)
-const alt = _.curry((fn1, fn2, val) => fn1(val) || fn2(val))
-
-// merge :: Either(a) -> a
-const merge = e => e.merge()
-
-// chain :: (ObjectA -> ObjectB), M -> ObjectB
-const chain = _.curry((fn, container) =>
-  container.chain(fn))
-
-const map = _.curry((fn, container) =>
-  container.map(fn))
 
 // eitherToTask :: Either -> Task
 const eitherToTask = e => e.fold(Task.rejected, Task.of)
 
 // getPropValue :: (String -> Object) -> Either
-const getPropValue = _.curry((prop, obj) =>
-  Either.fromNullable(_.prop(prop, obj)))
+const getPropValue = curry((p, obj) =>
+  Either.fromNullable(prop(p, obj)))
 
 // validate :: (RegEx -> String) -> Boolean
-const validate = _.curry((pattern, str) => pattern.test(str))
+const validate = curry((pattern, str) => pattern.test(str))
 
 // validateUrl :: Either(String) -> Either(String)
 const validateUrl = url =>
@@ -43,19 +29,8 @@ const findUrlData = url =>
       .then(data => result(Either.fromNullable(data)))
       .catch(err => reject(err)))
 
-// Impure randomGen -> seemingly random
-// const randomGen = _ => Math.floor(100000 + Math.random() * 900000)
-
-// subStr :: (Integer, Integer, String) -> String
-// const subStr = _.curry((start, end, str) => str.substring(start, end))
-
-// createShortUrl :: Integer -> String
-// const createShortUrl = _.compose(subStr(0, 4), _.toString)
-
-
 // getShortenedUrl :: Object -> Task
-const getShortenedUrl = _.compose(
-  map(merge),
+const getShortenedUrl = compose(
   chain(findUrlData),
   eitherToTask,
   chain(validateUrl),
