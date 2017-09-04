fca1acb12774080f5b89a7338b3962ba5c8c55b1
vinnya3 <vincent.aceto@gmail.com>
2017-09-04 14:35:31 -0400

    feat: change filename, default import & add new dir

--- models/Url/index.js
+++ models/Url/index.js
@@ -1,9 +1,9 @@
 import mongoose from 'mongoose'
 const {Schema} = mongoose
 
-const urlDataSchema = new Schema({
+const UrlSchema = new Schema({
   url: String,
   shortenedUrl: String
 })
 
-module.exports = mongoose.model('UrlData', urlDataSchema)
+module.exports = mongoose.model('Url', UrlSchema)
