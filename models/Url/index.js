08a070cc0cfb09a5cf97407a2f4c8cd03a50f72e
vinnya3 <vincent.aceto@gmail.com>
2017-09-05 17:02:44 -0400

    feat: add presave hook

--- models/Url/index.js
+++ models/Url/index.js
@@ -1,9 +1,16 @@
 import mongoose from 'mongoose'
+import { genRandom } from '../../utils'
 const {Schema} = mongoose
 
 const UrlSchema = new Schema({
-  url: String,
-  shortenedUrl: String
+  url: { type: String, required: true },
+  shortenedUrl: { type: String }
+})
+
+UrlSchema.pre('save', function (next) {
+  const random = genRandom(1000, 9999)
+  this.shortenedUrl = `${random}`
+  next()
 })
 
 module.exports = mongoose.model('Url', UrlSchema)
