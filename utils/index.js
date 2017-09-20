6d4604a32de8ecb80fa00f8330c525d487de2bd4
vinnya3 <vincent.aceto@gmail.com>
2017-09-20 16:49:08 -0400

    fix: export object

--- utils/index.js
+++ utils/index.js
@@ -1,3 +1,4 @@
 import asyncUrlActions from './asyncUrlActions'
 import helpers from './helpers'
-export default Object.assign({}, asyncUrlActions, helpers)
+
+export default Object.assign({}, helpers, asyncUrlActions)
