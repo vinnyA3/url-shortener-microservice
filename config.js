4fff7fecd3fc4c0a117fa5cbf48e38c73e1b3a55
vinnya3 <vincent.aceto@gmail.com>
2017-09-24 16:34:35 -0400

    feat: add hostname to config

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 module.exports = {
   port: process.env.PORT || 8080,
-  db: process.env.MONGOLAB_URI || 'mongodb://127.0.0.1:27017/url-shortener-service'
+  db: process.env.MONGOLAB_URI || 'mongodb://127.0.0.1:27017/url-shortener-service',
+  hostname: process.env.HOST || `localhost`
 }
