d00358a043507c74114d9edb77a3c631ef4fbda9
vinnya3 <vincent.aceto@gmail.com>
2017-09-25 01:08:47 -0400

    feat: add regex route def, imports
    Added regex for getShortenedUrl route. Imported new
    controllers. Added route for retrieveUrl.

--- routes/ApiRoutes.js
+++ routes/ApiRoutes.js
@@ -1,10 +1,15 @@
 import getShortenedUrl from '../controllers/ApiController'
+import retrieveUrl from '../controllers/retrieveUrl'
 
 export default (app, express) => {
   // create router instance
   const router = express.Router()
-  router.route('/*')
+  router.route(/^(?:\/)([\D]+)$/)
     .get(getShortenedUrl)
+
+  //router.route(/^(?:\/)(\d+)$/)
+  router.route('/:num')
+    .get(retrieveUrl)
   // return router obj
   return router
 }
