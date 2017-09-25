736ab9aa6cf70ea4fbe4b6afca26c3ed9c4e6b1f
vinnya3 <vincent.aceto@gmail.com>
2017-09-25 01:10:53 -0400

    feat: file rename

---
+++ controllers/getShortenedUrl.js
@@ -0,0 +1,38 @@
+'use strict'
+
+import { compose, curry, isNil, tap, map } from 'ramda'
+import { equals, maybeToEither, gets } from 'sanctuary'
+import utils from '../utils'
+
+const { testPattern, eitherToPromise, findUrlAsync,
+  createUrlAsync, then, catchP } = utils
+
+// eslint-disable-next-line
+const pattern =
+  /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/
+
+// validateUrl :: String -> Boolean
+const validateUrl = url => equals(testPattern(pattern, url), true)
+
+// safeGetAndValidate :: Object -> Maybe
+const safeGetAndValidate = req => gets(validateUrl, ['params', '0'], req)
+
+// validateToEither :: Object -> Either
+const validateToEither = compose(maybeToEither(`Invalid Url!`), safeGetAndValidate)
+
+// validateToPromise :: Object -> Promise
+const validateToPromise = compose(eitherToPromise, validateToEither)
+
+// findOrCreate :: String -> Promise
+const findOrCreate = url =>
+  compose(then(res => res || createUrlAsync(url)), tap(console.log), findUrlAsync)(url)
+
+// validateThenFindOrCreate :: Object(Request) -> Promise
+const validateThenFindOrCreate = compose(then(findOrCreate), validateToPromise)
+
+// export functionality and push side effects down pipe
+export default (req, res) => compose(
+  catchP(err => res.render('response', {title: 'Response', response: err})),
+  then(result => res.render('response', {title: 'Response', response: result})),
+  validateThenFindOrCreate
+)(req)
