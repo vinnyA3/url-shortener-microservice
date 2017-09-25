3a2da86e23c85cd6a5cab9c91e123d53929ea585
vinnya3 <vincent.aceto@gmail.com>
2017-09-25 11:15:33 -0400

    feat: remove host path refix

--- controllers/retrieveUrl.js
+++ controllers/retrieveUrl.js
@@ -2,18 +2,17 @@
 import { spawnSync } from 'child_process'
 import UrlData from '../models/Url'
 import utils from '../utils'
-import { compose, prop, curry, tap, props, chain, map } from 'ramda'
+import { compose, prop, curry, chain } from 'ramda'
 import { gets, is, maybeToEither } from 'sanctuary'
 
 const { then, catchP } = utils
 
-const safeGetAndIsString = req => gets(is(String), ['params', 'num'], req) 
+const safeGetAndIsString = req => gets(is(String), ['params', '0'], req) 
 
 const toEither = compose(maybeToEither('Not a valid short url!'), safeGetAndIsString)
 
-// refactor hardcoded uri for production
 const find = curry((db, shortUrl) =>
-  db.findOne({ shortenedUrl: `localhost:8080/api/${shortUrl}` }))
+  db.findOne({ shortenedUrl: shortUrl }))
 
 const findUrlAsync = shortUrl => find(UrlData, shortUrl)
 
