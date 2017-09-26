fcdfa8d46c7713668552e1c300a62b6a51b0611d
vinnya3 <vincent.aceto@gmail.com>
2017-09-26 15:33:07 -0400

    feat: move response handling into routes

--- microservice/routes/ApiRoutes.js
+++ microservice/routes/ApiRoutes.js
@@ -1,14 +1,22 @@
-import getShortenedUrl from '../controllers/getShortenedUrl'
-import retrieveUrl from '../controllers/retrieveUrl'
+import handlers from '../handlers'
+import { then, catchP } from '../utils'
+import { compose } from 'ramda'
 
 export default (app, express) => {
   // create router instance
   const router = express.Router()
+
   router.route(/^(?:\/)([\D]+)$/)
-    .get(getShortenedUrl)
+    .get((req, res) => compose(
+      catchP(err => res.send(err))
+      then(data => res.status(200).send(data))
+    )(req))
 
   router.route(/^(?:\/)(\d+)$/)
-    .get(retrieveUrl)
+    .get((req, res) => compose(
+      catchP(err => res.send(err))
+      then(date => res.status(200).send(data))
+    )(req))
 
   // return router obj
   return router
