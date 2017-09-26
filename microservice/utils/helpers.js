680a3aa45fcdda324decc855c8d7a6e44129a39b
vinnya3 <vincent.aceto@gmail.com>
2017-09-26 16:34:39 -0400

    refactor: remove export keywords for indv fns

--- microservice/utils/helpers.js
+++ microservice/utils/helpers.js
@@ -4,27 +4,27 @@ import { curry } from 'ramda'
 import { toEither, prop, either } from 'sanctuary'
 
 // getPropValue :: (String -> Object) -> Either
-export const safeGetProp = curry((p, obj) =>
-  toEither(`Prop: ${p} not found`, prop(p, obj)))
+const safeGetProp = curry((p, obj) =>
+toEither(`Prop: ${p} not found`, prop(p, obj)))
 
-export const then = curry((fn, thenable) => thenable.then(fn))
+const then = curry((fn, thenable) => thenable.then(fn))
 
-export const catchP = curry((fn, thenable) => thenable.catch(fn))
+const catchP = curry((fn, thenable) => thenable.catch(fn))
 
 // eitherToPromise :: Either -> Promise
-export const eitherToPromise = e => new Promise((resolve, reject) =>
-  either(v => reject(new Error(v)), resolve, e))
+const eitherToPromise = e => new Promise((resolve, reject) =>
+either(v => reject(new Error(v)), resolve, e))
 
 // testPattern :: (RegEx -> String) -> Boolean
-export const testPattern = curry((pattern, str) => pattern.test(str))
+const testPattern = curry((pattern, str) => pattern.test(str))
 
-export const alt = curry((fn1, fn2, val) => fn1(val) || fn2(val))
+const alt = curry((fn1, fn2, val) => fn1(val) || fn2(val))
 
 // genRandom :: Integer, Integer -> Integer
-export const genRandom = (min, max) =>
-  Math.floor(Math.random() * (max - min + 1)) + min
+const genRandom = (min, max) =>
+Math.floor(Math.random() * (max - min + 1)) + min
 
-export default {
+ export default {
 	safeGetProp,
 	then,
 	catchP,
