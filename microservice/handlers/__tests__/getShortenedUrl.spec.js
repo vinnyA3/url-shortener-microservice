6d874ff259ee950b27d0e08db4040ce3d5f17926
vinnya3 <vincent.aceto@gmail.com>
2017-09-28 08:10:43 -0400

    test: transform query toJSON

--- microservice/handlers/__tests__/getShortenedUrl.spec.js
+++ microservice/handlers/__tests__/getShortenedUrl.spec.js
@@ -1,18 +1,41 @@
+import mongoose from 'mongoose'
+import { prop } from 'ramda'
+import { db } from '../../config'
 import getShortenedUrl from '../getShortenedUrl'
+mongoose.Promise = global.Promise
+
+beforeAll(() => {
+  mongoose.connect(db)
+})
+
+afterAll((done) => {
+  mongoose.disconnect(done)
+})
 
 it('should return a promise', () => {
-  const url = 'http://www.google.com'
-  const req = requestFactory(url)
+  const req = requestFactory()
   const result = getShortenedUrl(req)
   return expect(result).toBeInstanceOf(Promise)
 })
 
+it('should resolved to an object', () => {
+  const doc = docFactory({
+    url: 'http://www.google.com', shortenedUrl: '5972'
+  })
+  const req = requestFactory()
+  const result = getShortenedUrl(req).then(d => d.toJSON())
+  return expect(result).resolves.toMatchObject(doc)
+})
 
 // requestFactory :: String -> Object
-function requestFactory(input) {
+function requestFactory (url = 'http://www.google.com') {
   return {
     params: {
-      '0': input
+      '0': url
     }
   }
+}
+
+function docFactory (fields) {
+  return Object.assign({}, fields)
 }
