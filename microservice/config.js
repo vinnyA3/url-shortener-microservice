1efd496b75bf760c6daec486ead1553cface3cdf
vinnya3 <vincent.aceto@gmail.com>
2017-09-28 17:21:08 -0400

    chore(config): fix mongo uri
    When deploying to Travis, original config
    was looking for the mongo env var in an
    uncommitted .env file. Instead, change to
    a hardcoded string.

--- microservice/config.js
+++ microservice/config.js
@@ -3,6 +3,6 @@ require('dotenv').config()
 
 module.exports = {
   port: process.env.PORT || 8080,
-  db: process.env.MONGOLAB_URI || process.env.DB_LOCAL,
+  db: process.env.MONGOLAB_URI || 'mongodb://127.0.0.1:27017/url-shortener-service',
   hostname: process.env.HOST || `localhost`
 }
