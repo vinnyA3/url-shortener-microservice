4097c3f48497fb0a0e9ba2b27b282c6420fc9259
vinnya3 <vincent.aceto@gmail.com>
2017-09-28 21:23:39 -0400

    test(retrieve): test retrievedUrl send back urldata

--- microservice/handlers/__tests__/retrieveUrl.spec.js
+++ microservice/handlers/__tests__/retrieveUrl.spec.js
@@ -1,4 +1,51 @@
-it('should return true', () => {
-  const result = true
-  expect(result).toBe(true)
+import mongoose from 'mongoose'
+import { db } from '../../config'
+import UrlData from '../../models/Url'
+import retrieveUrl from '../retrieveUrl'
+mongoose.Promise = global.Promise
+
+const testUrl = 'http://www.reddit.com'
+const testShortenedUrl = '8945'
+
+beforeAll(async () => {
+  mongoose.connect(db)
+  const newData = new UrlData({
+    url: testUrl,
+    shortenedUrl: testShortenedUrl
+  })
+  await newData.save(err => {
+    if (err) console.log(err)
+    else {
+      console.log('SAved!')
+    }
+  })
 })
+
+afterAll((done) => {
+  UrlData.remove({ url: testUrl })
+  mongoose.disconnect(done)
+})
+
+it('should return a promise', () => {
+  const req = requestFactory(testShortenedUrl)
+  const result = retrieveUrl(req)
+  return expect(result).toBeInstanceOf(Promise)
+})
+
+it('should resolve to a valid url object', () => {
+  const req = requestFactory(testShortenedUrl)
+  const result = retrieveUrl(req).then(d => d.toJSON())
+  return result.then(data => expect(data).toMatchObject({
+    url: testUrl,
+    shortenedUrl: testShortenedUrl
+  }))
+})
+
+// requestFactory :: String -> Object
+function requestFactory (shortenedUrl) {
+  return {
+    params: {
+      '0': shortenedUrl
+    }
+  }
+}
