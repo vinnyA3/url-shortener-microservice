a9a6f48142348f0e2e9b3213f1ad4bf3eaf1a628
vinnya3 <vincent.aceto@gmail.com>
2017-10-01 14:18:58 -0400

    feat: add global styles / wrapper + controls

--- client/src/index.js
+++ client/src/index.js
@@ -1,60 +1,19 @@
+// main global styles
+import 'styles/_main.scss'
 // inferno module
-import {render} from 'inferno';
+import {render} from 'inferno'
+// components
+import Wrapper from 'core/Wrapper'
+import MainDrawer from 'MainDrawer'
+import ResponseWindow from 'ResponseWindow'
 
-// routing modules
-import { Router, Link, Route, IndexRoute } from 'inferno-router';
-import createBrowserHistory from 'history/createBrowserHistory';
-
-const browserHistory = createBrowserHistory();
-function App({ children }) {
+const App = () => {
     return (
-		<div>
-			<h1>Application</h1>
-			<Link to="/users">User list</Link>
-			<br />
-			<Link to="/users/user/tester">Tester's page</Link>
-            {children}
-		</div>
+		<Wrapper>
+      <MainDrawer />
+      <ResponseWindow />
+		</Wrapper>
     )
 }
 
-function NoMatch({ children, params }) {
-    return (
-		<div>no match</div>
-    )
-}
-
-function Home({ children }) {
-    return (
-		<div>home</div>
-    )
-}
-
-// `children` in this case will be the `User` component
-function Users({ children, params }) {
-
-    return (
-		<div>
-			<h2>user list</h2>
-            {children}
-		</div>
-    )
-}
-
-function User({ params }) {
-    return <h1>{JSON.stringify(params)}</h1>
-}
-
-const routes = (
-	<Router history={browserHistory}>
-		<Route component={App}>
-			<IndexRoute component={Home} />
-			<Route path="/users" component={Users}>
-				<Route path="/user/:username" component={User} />
-			</Route>
-			<Route path="*" component={NoMatch} />
-		</Route>
-	</Router>
-);
-
-render(routes, document.getElementById('app'));
+render(<App />, document.getElementById('app'))
