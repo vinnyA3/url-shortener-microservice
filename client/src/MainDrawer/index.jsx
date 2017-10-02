ad908b6db02ada2bab7a0dfdf2003c9286a6ca83
vinnya3 <vincent.aceto@gmail.com>
2017-10-02 08:33:04 -0400

    feat: add Response component

--- client/src/MainDrawer/index.jsx
+++ client/src/MainDrawer/index.jsx
@@ -1,10 +1,12 @@
 import './_styles.scss'
 import Controls from './scenes/Controls'
+import Response from './scenes/Response'
 
 const MainDrawer = () => {
   return (
     <div className='main-drawer'>
       <Controls />
+      <Response />
     </div>
   )
 }
