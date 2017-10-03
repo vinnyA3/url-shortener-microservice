7f5f4794ac8b54014a5e8b8375e4b36e18445717
vinnya3 <vincent.aceto@gmail.com>
2017-10-03 10:22:58 -0400

    feat: curry most functions for pointfree programming

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -1,7 +1,10 @@
-import { map, chain } from 'most'
+import { map, chain, recoverWith, throwError } from 'most'
 import { curry } from 'ramda'
 
 export const curriedMap = curry(map)
 
 export const curriedChain = curry(chain)
 
+export const curriedRecover = curry(recoverWith)
+
+export const curriedThrow = curry(throwError)
