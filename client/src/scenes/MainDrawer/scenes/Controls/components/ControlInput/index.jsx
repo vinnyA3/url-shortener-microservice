c16b3e0fc8aafa91298bc3faf7288e9fff6530e1
vinnya3 <vincent.aceto@gmail.com>
2017-10-03 21:08:43 -0400

    feat: remove sass file + add glam styles

--- client/src/scenes/MainDrawer/scenes/Controls/components/ControlInput/index.jsx
+++ client/src/scenes/MainDrawer/scenes/Controls/components/ControlInput/index.jsx
@@ -1,6 +1,15 @@
+import { Input } from 'glamorous'
+
 const ControlInput = ({updateInput}) => {
   return (
-    <input 
+    <Input
+      color='#D89217'
+      border='.02em solid orange'
+      paddingTop='1em'
+      paddingBottom='1em'
+      paddingLeft='1em'
+      borderTopLeftRadius='.4em'
+      borderBottomLeftRadius='.4em'
       placeholder='Your Url Here...'
       onInput={updateInput}
     />
