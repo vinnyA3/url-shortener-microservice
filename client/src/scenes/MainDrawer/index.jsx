93d82e7d6e06d4f2db8f3b0e6b4ef3e6b6c19e3a
vinnya3 <vincent.aceto@gmail.com>
2017-10-05 20:50:04 -0400

    feat: import image, style bg, add dummy txt

--- client/src/scenes/MainDrawer/index.jsx
+++ client/src/scenes/MainDrawer/index.jsx
@@ -1,6 +1,7 @@
 import { Div } from 'glamorous'
 import Controls from './scenes/Controls'
 import Response from './scenes/Response'
+import nyc from './images/nyc.jpg'
 
 const MainDrawer = ({height, width, color}) => {
   return (
@@ -12,7 +13,12 @@ const MainDrawer = ({height, width, color}) => {
       height={height} 
       width={width} 
       backgroundColor={color}
+      backgroundImage={`url(${nyc})`}
+      backgroundSize='cover'
+      color='#fff'
     >
+      <h1>Welcome to tIne.io</h1>
+      <p>Enter you desired url to enjoy all the goodness a shortened url has to offer</p>
       <Controls />
       <Response />
     </Div>
