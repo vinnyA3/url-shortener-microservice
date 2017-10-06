1ad76cd47681c15d4fa54b53b36e66fc9485a06e
vinnya3 <vincent.aceto@gmail.com>
2017-10-06 10:51:50 -0400

    chore(config): add hostname + mongouri

--- microservice/config.js
+++ microservice/config.js
@@ -4,5 +4,5 @@ require('dotenv').config()
 module.exports = {
   port: process.env.PORT || 8080,
   db: process.env.MONGOLAB_URI || 'mongodb://127.0.0.1:27017/url-shortener-service',
-  hostname: process.env.HOST || `localhost`
+  hostname: process.env.HOST || 'http://localhost:8080/api'
 }
