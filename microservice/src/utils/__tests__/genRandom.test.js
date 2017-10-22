60b9cea066071d1307a1da2a909bbe7dd6f3261f
vinnya3 <vincent.aceto@gmail.com>
2017-10-21 22:26:47 -0400

    test: refactor test to check for string and len

--- microservice/src/utils/__tests__/genRandom.test.js
+++ microservice/src/utils/__tests__/genRandom.test.js
@@ -2,13 +2,9 @@ import utils from '../index'
 const { genRandom } = utils
 
 describe('helpers testing', () => {
-  it('genRandom should gen random int between min/max', () => {
-    const min = 10
-    const max = 20
-    const result = genRandom(min, max)
-
-    expect(typeof result).toBe('number')
-    expect(result).toBeGreaterThanOrEqual(min)
-    expect(result).toBeLessThanOrEqual(max)
+  it('genRandom should gen random string of len 5', () => {
+    const result = genRandom()
+    expect(typeof result).toBe('string')
+    expect(result).toHaveLength(5)
   })
 })
