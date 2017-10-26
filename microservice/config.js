82d3385ead37c425b85523fc7359d3cc5eeabce0
vinnya3 <vincent.aceto@gmail.com>
2017-10-25 23:15:50 -0400

    chore(config): update local mongo uri

--- microservice/config.js
+++ microservice/config.js
@@ -2,7 +2,7 @@
 require('dotenv').config()
 
 module.exports = {
-  port: process.env.PORT || 8080,
-  db: process.env.MONGOLAB_URI || 'mongodb://127.0.0.1:27017/url-shortener-service',
-  hostname: process.env.HOST || 'http://localhost:8080/api'
+  port: process.env.PORT || 8000,
+  db: process.env.MONGOLAB_URI || process.env.DB_LOCAL,
+  hostname: process.env.HOST || 'http://localhost:8000/api'
 }
